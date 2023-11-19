7581fee1f42e8bdf49e92737a46836a1ea0387ae
Mateus A. Carvalho <mateusabreu94@hotmail.com>
2023-11-19 17:29:32 -0300

    All styles have been done. I applied the same ripple effect in previous project. I came across a error in index.js. I used template literals but continues using the same concatenates syntax. Now, I finished the styles and structures. Passing to the script code.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,6 +4,6 @@ btnEl.addEventListener("mouseover", (e) => {
   let xPos = (e.pageX - btnEl.offsetLeft);
   let yPos = (e.pageY - btnEl.offsetTop);
 
-  btnEl.style.setProperty("--xPos", `${xPos} + px`)
-  btnEl.style.setProperty("--yPos", `${yPos} + px`)
+  btnEl.style.setProperty("--xPos", `${xPos}px`)
+  btnEl.style.setProperty("--yPos", `${yPos}px`)
 })
