d9e7c8d690c70dc5da64f059e5cdcf42e67704ca
Mateus A. Carvalho <mateusabreu94@hotmail.com>
2023-11-20 20:53:13 -0300

    Script - Adding play when click in button
    Today I added the feature to show the video when the button is clicked. To do this, I set some properties in video html tag to change this when the click event is trigged. The visibility property was one of these property that I used to make the video appear. Now, I have to fix somes settings in animations syntax to make the video appear smothly, some settings in widht and height to user see controls and make something to close the video. I'm thinking to add some div and caught this with JS or some input with his pseudo-elements and make this only in the CSS.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,5 @@
 const btnEl = document.querySelector('#btn');
+const trailerEl = document.querySelector('#trailer');
 
 btnEl.addEventListener("mouseover", (e) => {
   let xPos = (e.pageX - btnEl.offsetLeft);
@@ -7,3 +8,15 @@ btnEl.addEventListener("mouseover", (e) => {
   btnEl.style.setProperty("--xPos", `${xPos}px`)
   btnEl.style.setProperty("--yPos", `${yPos}px`)
 })
+
+btnEl.addEventListener("click", () => {
+  play(trailerEl);
+})
+
+function play(element) {
+  element.play();
+  element.style.visibility = "visible"
+  element.style.width = "90vw"
+  element.style.height = "90vw"
+  element.style.zIndex = "10"
+}
