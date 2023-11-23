066cf041e6a8273aae0206a8eda20dafd74826a3
Mateus A. Carvalho <mateusabreu94@hotmail.com>
2023-11-23 06:19:58 -0300

    Close button function implemented Finally I make this function. I choose don't use a section to wrapper the elements. Somehow, it was broking my project. Instead, I used just a span with some properties and put him in the superior corner of the window. Now, the project has been finished.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,8 +2,8 @@ const btnEl = document.querySelector('#btn');
 const trailerEl = document.querySelector('#trailer');
 const bodyEl = document.querySelector('body');
 const closeTrailerEl = document.querySelector('#close-btn');
-const trailerWrapperEl = document.querySelector(".trailer-wrapper");
 
+// Ripple Button Effect Function
 btnEl.addEventListener("mouseover", (e) => {
   let xPos = (e.pageX - btnEl.offsetLeft);
   let yPos = (e.pageY - btnEl.offsetTop);
@@ -12,27 +12,33 @@ btnEl.addEventListener("mouseover", (e) => {
   btnEl.style.setProperty("--yPos", `${yPos}px`)
 })
 
+// Close clicking in somewhere of body 
 bodyEl.addEventListener("click", (e) => {
   let event = e.target
-  if(event === bodyEl) return reloadTrailer(trailerEl);
+  if(event === bodyEl || event === closeTrailerEl) return reloadTrailer(trailerEl, closeTrailerEl);
 })
 
+// Play video event trigger
 btnEl.addEventListener("click", () => {
-  trailerWrapperEl.style.display = "block";
-  playTrailer(trailerEl);
+  playTrailer(trailerEl, closeTrailerEl);
 })
 
-function playTrailer(element, closeEl) {
+// Play video function
+function playTrailer(element, closeBtn) {
   element.play();
   element.style.visibility = "visible"
+  closeBtn.style.visibility = "visible"
   element.style.width = "85vw"
   element.style.height = "85vh"
   element.style.zIndex = "10"
   element.classList.add('fade-in');
+  closeBtn.classList.add('fade-in');
 }
 
-function reloadTrailer(element) {
+// Stop video function
+function reloadTrailer(element, closeBtn) {
   element.pause();
   element.currentTime = 0;
   element.style.visibility = "hidden"
+  closeBtn.style.visibility = "hidden"
 }
